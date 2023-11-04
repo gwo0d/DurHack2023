ef6c8a05c375e5c5ff064931fe82119fff2b376e
KHajzner <91436731+KHajzner@users.noreply.github.com>
2023-11-04 20:43:39 +0000

    Eating Animation (#8)
    Added animation for head opening, eating the text, head closing

---
+++ Frontend/main.js
@@ -0,0 +1,9 @@
+function submission(){
+    //Head Animation
+    var head = document.getElementById("top-half");
+    head.classList.toggle("transform-active");
+
+    //Text Animation
+    var text = document.getElementById("form");
+    text.classList.toggle("transformation-active");
+}
