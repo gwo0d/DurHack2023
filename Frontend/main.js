87204ef20c04e2c02c9d11816c86c0ef4581ffbd
KHajzner <khajzner@gmail.com>
2023-11-04 17:49:58 +0000

    Image Animation

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,4 +1,8 @@
 function submission(){
     let title = document.getElementById('title').value;
     console.log('Submitted!' + title);
+    var element = document.getElementById("top-half");
+    element.classList.toggle("transform-active");
+    var text = document.getElementById("form");
+    text.classList.toggle("transformation-active");
 }
