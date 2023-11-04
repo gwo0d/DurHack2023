bf0fb1e38f27e39b4cb2bf452d3cf1ae0932d5ed
KHajzner <khajzner@gmail.com>
2023-11-04 21:12:47 +0000

    Error message

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,9 +1,17 @@
 function submission(){
-    //Head Animation
-    var head = document.getElementById("top-half");
-    head.classList.toggle("transform-active");
+    var empty =  document.getElementById("empty-input");
+    if(!(document.getElementById("title").value)){
+        empty.style.display = "block";
+        return;
+    }
+    else{
+        empty.style.display = "none";
+        //Head Animation
+        var head = document.getElementById("top-half");
+        head.classList.toggle("transform-active");
 
-    //Text Animation
-    var text = document.getElementById("form");
-    text.classList.toggle("transformation-active");
+        //Text Animation
+        var text = document.getElementById("form");
+        text.classList.toggle("transformation-active");
+    }
 }
