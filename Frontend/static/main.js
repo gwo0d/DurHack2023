9abeadfdd8c9c2a0c4dd1a83c3911cbb261b0a70
KHajzner <91436731+KHajzner@users.noreply.github.com>
2023-11-05 03:31:52 +0000

    Output (#13)
    * Head shake
    
    * chrome modal
    
    * Minor fixes

--- Frontend/static/main.js
+++ Frontend/static/main.js
@@ -3,19 +3,26 @@ function add_image(){
     img.src = "/static/ai.png"
     document.getElementById('header-image').appendChild(img);
 }
+function image_animation(){
+    document.getElementById("top-half").classList.remove("head-shake");
+    setTimeout(reset, 1000);
+    setTimeout(add_image, 2500);
+}
 
+//TODO: HAVE TO UNCOMMENT THIS STUFF, REMOVE THE `setTimeout(image_animation, 8000);` bit
 function request_image(){
     var query = document.getElementById("title");
     var http = new XMLHttpRequest();
-    http.open("POST", "/query", true);
-    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    var params = "prompt=" + query.value; // probably use document.getElementById(...).value
-    http.send(params);
-    http.onload = function() {
-        add_image()
-    }
-    setTimeout(reset, 30000); 
-    setTimeout(output, 30000); 
+    shaking();
+    // http.open("POST", "/query", true);
+    // http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    // var params = "prompt=" + query.value; // probably use document.getElementById(...).value
+    // http.send(params);
+    // http.onload = function() {
+    //     image_animation()
+    // }
+    setTimeout(image_animation, 8000);
+
 }
 
 function submission(){
@@ -32,15 +39,26 @@ function submission(){
         //Text Animation
         var text = document.getElementById("form");
         text.classList.toggle("transformation-active");
+        setTimeout(removeEatingAnimation, 3000);
         request_image()
     }
 }
 
+//Head shake while loading
+function shaking(){
+    var head = document.getElementById("top-half");
+    head.classList.toggle("head-shake");
+}
 function reset(){
     var head = document.getElementById("top-half");
     head.classList.toggle("transform-active");
 }
-function output(){
-    var head = document.getElementById("top-half");
-    head.classList.toggle("transform-active");
+
+var modal = document.getElementById("modal");
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+function removeEatingAnimation(){
+    document.getElementById("top-half").classList.remove("transform-active");
 }
