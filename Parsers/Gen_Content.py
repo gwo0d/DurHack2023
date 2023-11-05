a633cb1132ac22a215166f213d22e5cf6b514cb3
Evangeline Jackson <54007582+eviejackson@users.noreply.github.com>
2023-11-05 09:55:01 +0000

    Add ChatGPT generation

--- Parsers/Gen_Content.py
+++ Parsers/Gen_Content.py
@@ -15,10 +15,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-### File interfacing with chatGPT to summarise content in a directory.
-### This content happens to be Monty Python scripts
+### ChatGPT interface to generate monty python scripts
 class LLMAccessor(ABC):
-    prompt_instructions = """ Summarise each provided screenplay script into 2 sentences. """
+    prompt_instructions = """ Write a novel Monty Python screenplay. """
     text_prompt = "text: What is your actual text answer to the prompt? Do not provide any other parameters. Only " \
                   "respond with your verbatim text answer to the prompt."
 
@@ -56,24 +55,24 @@ class LLMAccessor(ABC):
     def model(self, m):
         self._model = m
 
-    def generate_prompts(self, script) -> dict[str, str]:
+    def generate_prompts(self, user_prompt) -> dict[str, str]:
         text_template = f"""
-            Please give a 2 sentence summary for the following text:
+            Please give a full script for the following text:
 
-            text: {script}
+            text: {user_prompt}
 
-            Format the response as JSON wrapped in curly brackets. JSON should have the following keys:
-            - summary
-            - full_script
+            Format the response as plain text. Each time a speaker says a line, the speaker's name should be in all 
+            capitals and on a new line. The text a speaker says should be immediately below the speaker's name on a new
+            line. This might look as follows:
             
-            The key, summary, should have your generated summary - the output. The key, full_script, should have the 
-            input {script}
-            """
+                JOHN CLEESE:
+                "Arthur, Arthur, King of the Britons, you have provided yourself worthy."
 
-        summary_schema = ResponseSchema(
-            name="summary",
-            description=""
-        )
+                LANCELOT:
+                "Right. Who's with me?"
+            
+            The script should be 1970's tongue in cheek british humour, in the Monty Python style, based on the user input.
+            """
 
         script_schema = ResponseSchema(
             name="script",
@@ -81,7 +80,6 @@ class LLMAccessor(ABC):
         )
 
         response_schemas = [
-            summary_schema,
             script_schema
         ]
 
@@ -100,28 +98,13 @@ class LLMAccessor(ABC):
         print("*****************THIS IS THE RESPONSE***********************")
         print(response)
 
-        #print("This is get parameters output dict:")
-        #output_dict = output_parser.parse(response.content)
-        #print(output_dict)
-
         return response
 
 
 if __name__ == "__main__":
-    folder = "../training_data/data"
+
     Accessor = LLMAccessor()
 
-    for filename in os.listdir(folder):
-        if filename.endswith(".txt"):
-
-            ## Get script data for script
-            fullyqualified_path = folder + "/" + filename
-            _input: str = ""
-            with open(fullyqualified_path, 'r') as f:
-                for line in f.readlines():
-                    _input += line
-            f.close()
-
-            f = open("output.txt", "a")
-            f.write(str(Accessor.generate_prompts(_input)))
-            f.close()
+    f = open("output.txt", "a")
+    f.write(str(Accessor.generate_prompts(input("Please give a prompt"))))
+    f.close()
